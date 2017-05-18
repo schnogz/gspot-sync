4b46f1132fe1cfe0b0239ce11eba07e34b9574c7
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-05-18 00:36:13 -0500

    set up static content routing in production

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const http = require('http');
 const path = require('path');
 const methods = require('methods');
 const requestIp = require('request-ip');
-const session = require('express-session');
 
 var isProduction = process.env.NODE_ENV === 'production';
 var app = express();
@@ -19,25 +18,17 @@ app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('method-override')());
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 app.use(require('./routes/index'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
 
 // development config
 if (!isProduction) {
   // error handler will print stacktrace
   app.use(errorhandler());
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     console.log(err.stack);
-
     res.status(err.status || 500);
-
     res.json({'errors': {
       message: err.message,
       error: err
@@ -46,12 +37,10 @@ if (!isProduction) {
 } else {
   // production config
   // serve built static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  }
+  app.use(express.static(path.join(__dirname, '../build')));
 
   // production error handler w/o stacktraces
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.json({'errors': {
       message: err.message,
@@ -62,6 +51,12 @@ if (!isProduction) {
 
 // start server
 var server = app.listen(process.env.PORT || 3001, function() {
-  console.log('Express server listening on port ' + server.address().port + '!');
-  console.log('Remember! Webpack will proxy requests to /api on port 3000 to Express.');
+  if (!isProduction) {
+    console.log('Express server environment mode: DEV');
+    console.log('Express server listening port: ' + server.address().port);
+    console.log('REMINDER! Webpack will proxy requests to /api on port 3000 to Express.');
+  } else {
+    console.log('Express server environment mode: PROD');
+    console.log('Express server listening port: ' + server.address().port);
+  }
 });
