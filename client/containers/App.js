04fc7c69530aed4f4793579f64212b7b5a02693d
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-05-19 22:09:42 -0500

    crude http request to test setup in heroku

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-
+import 'whatwg-fetch';
+/*
 const App = () => (
   <MuiThemeProvider>
     <RaisedButton label="Hello World!" />
   </MuiThemeProvider>
 );
+
+export default App;
+*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      concerts: []
+    };
+  }
+
+  componentDidMount() {
+    let that = this;
+    fetch('/api/songkick/search/events', {}).then(function(response) {
+      response.json().then(function(data) {
+        const concerts = data;
+        that.setState({ concerts });
+      });
+    }, function(error) {
+      // handle network error
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Concerts in Minneapolis</h1>
+        <ul>
+          {this.state.concerts.map(concert =>
+            <li key={concert.id}>{concert.displayName}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
--- client/index.js
+++ client/index.js
@@ -6,12 +6,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './containers/App';
 import './styles/index.css';
 
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
+// needed for Mui
 injectTapEventPlugin();
 
-
-const rootEl = document.getElementById('root');
-
-ReactDOM.render(<App />, rootEl);
+// load the app
+ReactDOM.render(<App />, document.getElementById('root'));
