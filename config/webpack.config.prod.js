bd745a798dbb1368876e1974597900c99a7f68ad
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-05-20 21:03:22 -0500

    auto fixing lint issues and added lint caching

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -15,7 +15,7 @@ const getClientEnvironment = require('./env');
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 // TODO: the line below fixed production paths but might be an issue...?
-const publicPath = '/'; //paths.servedPath;
+const publicPath = '/'; // paths.servedPath;
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === './';
--- scripts/lint-exec.js
+++ scripts/lint-exec.js
@@ -2,9 +2,15 @@
 
 const CLIEngine = require('eslint').CLIEngine;
 
-const cli = new CLIEngine();
+const cli = new CLIEngine({
+  cache: true,
+  fix: true
+});
 
 process.on('message', (patterns) => {
   const report = cli.executeOnFiles(patterns);
+  // output fixes to disk
+  CLIEngine.outputFixes(report);
+
   process.send([report, cli.getFormatter()(report.results)]);
 });
